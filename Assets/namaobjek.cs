1be2d89e8843cf4c401f7beba2bf5c8cb6bfc1a3
oskaaditya <oska.aditya18@gmail.com>
2020-12-30 19:43:16 +0700

    UAS 2 Pemrograman Game

---
+++ Assets/namaobjek.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class namaobjek : MonoBehaviour
+{
+    public Text Textfield;
+
+    public void SetText(string text)
+    {
+        Textfield.text = text;
+    }
+}
